7cea41c9110bab0ee23eec9acb220676c26b309e
Sem <22419023+Nickless09@users.noreply.github.com>
2025-11-12 14:29:10 +0100

    Update test_cpp_integration.py

--- tests/test_cpp_integration.py
+++ tests/test_cpp_integration.py
@@ -1,4 +1,12 @@
+import sys
+import pytest
+
+# Skip on Python2 because C-extension is built only for Python3 in CI
+pytestmark = pytest.mark.skipif(
+    sys.version_info < (3, 0),
+    reason="C extension built only for Python 3 in CI"
+)
+
 def test_arithmetic_import():
-    # This will import python package which expects a compiled extension file arithmetic.so
     from arithmetic_pkg import sum
     assert sum(10, 11) == 21
